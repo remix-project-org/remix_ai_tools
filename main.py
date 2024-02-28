941d4f8e62ec9fef6d2a421d3476ace4b2c655dd
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-02-28 18:49:37 +0100

    rm app middleware cors

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
 from fastapi import FastAPI
 from deploy_service import gr, gr_app
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-origins = ["https://remix.beta.ethereum.org",
-            "https://remix.alpha.ethereum.org",
-            "https://remix.ethereum.org",
-            "http://localhost:7861",
-            "http://127.0.0.1:7861"
-        ]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 @app.get("/")
 def read_main():
