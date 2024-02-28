7aa395237a5c693840933a628dccec7c5936b55e
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-02-28 19:24:10 +0100

    queuing

--- deploy_service.py
+++ deploy_service.py
@@ -13,7 +13,7 @@ codecomp = gr.Interface(
     outputs=["text"],
     title="Code Completion",
     api_name="code_completion",
-    concurrency_limit=7,
+    concurrency_limit=1,
 )
 
 codegen = gr.Interface(
@@ -71,5 +71,5 @@ gr_app = gr.TabbedInterface([codecomp, codegen, codeexp, errorexp, contractgen, 
                           "error_explaining", "contract_generation",
                           "solidity_answer"])
 
-gr_app.queue(max_size=100, default_concurrency_limit=7)#.launch(share=True, root_path="/ai-tools", max_threads=50, show_api=False)
+gr_app.queue(max_size=100, default_concurrency_limit=1)#.launch(share=True, root_path="/ai-tools", max_threads=50, show_api=False)
 # app.queue(5).launch(share=True, root_path="/ai-tools", auth=(os.getenv('GRADIO_ADMIN'), os.getenv('GRADIO_ADMIN_PASS')))
