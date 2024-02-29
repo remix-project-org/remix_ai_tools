0293f32f165cbb1f6c6ecdb6a6c2f9a482380caf
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-02-29 16:56:32 +0100

    reverted body

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ async def code_completion(context_code: str=Body(),
 
 @app.post("/ai/api/code_generation")
 async def code_generation(context_code:str=Body(),
+    comment: str= Body(),
     stream_result: bool=True,
     max_new_tokens: int = 1000,
     temperature: float = 0.1,
