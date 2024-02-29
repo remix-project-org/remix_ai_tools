12e9021daf484a97df2c33eb19b6901776ede1ce
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-02-29 11:24:43 +0100

    code completion no gradio

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Body
 #from deploy_service import gr, gr_app
 from src.model_inference_cpp import *
 
@@ -10,8 +10,8 @@ def read_main():
     return {"message": "Welcome to REMIX-IDE AI services"}
 
 @app.post("/ai/api/code_completion")
-async def code_completion(context_code: str=Query(None),
-    comment: str= Query(None),
+async def code_completion(context_code: str=Body(),
+    comment: str= Body(),
     stream_result: bool=True,
     max_new_tokens: int = 1024,
     temperature: float = 0.1,
