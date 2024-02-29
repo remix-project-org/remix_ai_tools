7649e789b2f6165665fe124b9f8a9ae4c48e4dbd
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-02-29 11:20:57 +0100

    code completion no gradio

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ def read_main():
     return {"message": "Welcome to REMIX-IDE AI services"}
 
 @app.post("/ai/api/code_completion")
-async def code_completion(context_code: str=Query(),
-    comment: str= Query(),
+async def code_completion(context_code: str=Query(...),
+    comment: str= Query(None),
     stream_result: bool=True,
     max_new_tokens: int = 1024,
     temperature: float = 0.1,
     top_p: float = 0.9,
     top_k: int = 50):
     print('INFO - Code Completion:', context_code)
+    print('INFO - Code Completion:', comment)
     return run_code_completion(context_code, comment, stream_result, max_new_tokens, temperature, top_p, top_k)
 
 #app = gr.mount_gradio_app(app, gr_app, path="/ai")
