b6bb431d8e55f6a6f8856aa567b0f7f68ab08a55
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-02-29 17:32:29 +0100

    rid of body

--- src/model_inference_cpp.py
+++ src/model_inference_cpp.py
@@ -173,6 +173,7 @@ def run_answering(
         )
 
         outputs = model(**generate_kwargs)
+        text = outputs["choices"][0]["text"].strip()
         text = get_string_between(text, "```", "```") if '```' in text else text
         return text
     except Exception as ex:
