16a4b6ed9b5ed8b893e0789280123572fd9f7752
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-03-01 10:24:29 +0100

    using mistral

--- ReadMe.md
+++ ReadMe.md
@@ -6,3 +6,15 @@ Implements AI endpoints for the Remix-IDE
 ### Code Generation 
 ### Code Explaining
 ### Error Correction and Explaining 
+
+### Run server
+
+```bash
+git fetch && git pull && gunicorn main:app --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:7860 --access-logfile - --workers 2 --threads 1 --timeout 600
+```
+
+### Install requirements 
+```bash 
+pip install -r requirements.txt
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python
+```
--- load_test.py
+++ load_test.py
@@ -10,59 +10,59 @@ class QuickstartUser(HttpUser):
 
     @task
     def test_code_completion(self):
-        self.client.post( "https://hkfll35zthu6e2-7860.proxy.runpod.net/ai/api/code_completion", 
-        json={
-            "context_code":"// SPDX-License-Identifier: GPL-3.0\n pragma solidity >=0.8.2 <0.9.0;\n\n/**\n * @title Storage\n * @dev Store & retrieve value in a variable\n * @custom:dev-run-script ./scripts/deploy_with_ethers.ts\n */\ncontract Storage {\n\n    uint256 number;",
+        self.client.post( "https://l70m88pijz3d0h-7860.proxy.runpod.net/ai/api/code_completion", 
+        json={  # convert dictionary to string
+            "context_code":"// SPDX-License-Identifier: GPL-3.0\n pragma solidity >=0.8.2 <0.9.0;\n\n\ncontract Storage {\n\n    uint256 number; \n\n    ///write a function for adding 2 uint256 numbers and return the result",
             "comment":"",
             "max_new_tokens":10
         })
 
-    @task
-    def test_code_generation(self):
-        self.client.post( "https://hkfll35zthu6e2-7860.proxy.runpod.net/ai/api/code_completion", 
-        json=json.dumps({
-            "context_code":"// SPDX-License-Identifier: GPL-3.0\n pragma solidity >=0.8.2 <0.9.0;\n\n\ncontract Storage {\n\n    uint256 number; \n\n    ///write a function for adding 2 uint256 numbers and return the result",
-            "comment":""
-        }))
+    # @task
+    # def test_code_generation(self):
+    #     self.client.post( "https://l70m88pijz3d0h-7860.proxy.runpod.netai/api/code_completion", 
+    #     json=json.dumps({
+    #         "context_code":"// SPDX-License-Identifier: GPL-3.0\n pragma solidity >=0.8.2 <0.9.0;\n\n\ncontract Storage {\n\n    uint256 number; \n\n    ///write a function for adding 2 uint256 numbers and return the result",
+    #         "comment":""
+    #     }))
 
 
-    @task
-    def test_code_explaining(self):
-        self.client.post( "https://hkfll35zthu6e2-7860.proxy.runpod.net/ai/api/code_explaining", 
-        json=json.dumps({
-            "context_code":"// SPDX-License-Identifier: GPL-3.0\n pragma solidity >=0.8.2 <0.9.0;\n\n/**\n * @title Storage\n * @dev Store & retrieve value in a variable\n * @custom:dev-run-script ./scripts/deploy_with_ethers.ts\n */\ncontract Storage {\n\n    uint256 number;",
-        }))
+    # @task
+    # def test_code_explaining(self):
+    #     self.client.post( "https://l70m88pijz3d0h-7860.proxy.runpod.netai/api/code_explaining", 
+    #     json=json.dumps({
+    #         "context_code":"// SPDX-License-Identifier: GPL-3.0\n pragma solidity >=0.8.2 <0.9.0;\n\n/**\n * @title Storage\n * @dev Store & retrieve value in a variable\n * @custom:dev-run-script ./scripts/deploy_with_ethers.ts\n */\ncontract Storage {\n\n    uint256 number;",
+    #     }))
 
 
-    @task
-    def test_error_explaining(self):
-        self.client.post( "https://hkfll35zthu6e2-7860.proxy.runpod.net/ai/api/error_explaining", 
-        json=json.dumps({
-            "err":"ParserError: Expected pragma, import directive or contract/interface/library/struct/enum/constant/function/error definition.--> contracts/1_Storage.sol:42:5:"
-        }))
+    # @task
+    # def test_error_explaining(self):
+    #     self.client.post( "https://l70m88pijz3d0h-7860.proxy.runpod.netai/api/error_explaining", 
+    #     json=json.dumps({
+    #         "err":"ParserError: Expected pragma, import directive or contract/interface/library/struct/enum/constant/function/error definition.--> contracts/1_Storage.sol:42:5:"
+    #     }))
 
 
-    @task
-    def test_contract_generation(self):
-        self.client.post( "https://hkfll35zthu6e2-7860.proxy.runpod.net/ai/api/contract_generation", 
-        json=json.dumps({
-            "desc":"Write a contract for a simple storage"
-        }))
+    # @task
+    # def test_contract_generation(self):
+    #     self.client.post( "https://l70m88pijz3d0h-7860.proxy.runpod.netai/api/contract_generation", 
+    #     json=json.dumps({
+    #         "desc":"Write a contract for a simple storage"
+    #     }))
 
-    @task
-    def test_answering(self):
-        self.client.post( "https://hkfll35zthu6e2-7860.proxy.runpod.net/ai/api/answering", 
-        json=json.dumps({
-            "question":"What is the purpose of the storage contract?"
-        }))
+    # @task
+    # def test_answering(self):
+    #     self.client.post( "https://l70m88pijz3d0h-7860.proxy.runpod.netai/api/answering", 
+    #     json=json.dumps({
+    #         "question":"What is the purpose of the storage contract?"
+    #     }))
         
     def on_start(self):
         
         print('started')
 
-x = requests.post(url="https://hkfll35zthu6e2-7860.proxy.runpod.net/ai/api/code_completion", 
-        json={
-            "context_code":"// SPDX-License-Identifier: GPL-3.0\n pragma solidity >=0.8.2 <0.9.0;\n\n/**\n * @title Storage\n * @dev Store & retrieve value in a variable\n * @custom:dev-run-script ./scripts/deploy_with_ethers.ts\n */\ncontract Storage {\n\n    uint256 number;",
+x = requests.post("https://l70m88pijz3d0h-7860.proxy.runpod.net/ai/api/code_completion", 
+        json={  # convert dictionary to string
+            "context_code":"// SPDX-License-Identifier: GPL-3.0\n pragma solidity >=0.8.2 <0.9.0;\n\n\ncontract Storage {\n\n    uint256 number; \n\n    ///write a function for adding 2 uint256 numbers and return the result",
             "comment":"",
             "max_new_tokens":10
         })
