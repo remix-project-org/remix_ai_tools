aeb23dbdeb6d0d411e0f6861600d5553f549ca9c
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-03-04 12:05:29 +0100

    using main code generation

--- src/model_inference_cpp.py
+++ src/model_inference_cpp.py
@@ -62,6 +62,7 @@ def run_code_generation(
 
         outputs = model(**generate_kwargs)
         text = outputs["choices"][0]["text"].strip()
+        text = get_string_between(text, "```", "```") if '```' in text else text
         return text
     except Exception as ex:
         print('ERROR - Code generation', ex)
