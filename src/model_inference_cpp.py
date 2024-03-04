3f3b37460ae8a6ab1ad0ccd12f072b674fcbbbaa
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-03-04 12:39:02 +0100

    added basic stops

--- src/model_inference_cpp.py
+++ src/model_inference_cpp.py
@@ -30,7 +30,7 @@ def run_code_completion(
             top_p=top_p,
             top_k=top_k,
             temperature=temperature,
-            stop=["\n"]
+            stop=[" \n", "<|endoftext|>", "</s>", "\t", "  "]
         )
         outputs = model(**generate_kwargs)
         text = outputs["choices"][0]["text"].strip()
