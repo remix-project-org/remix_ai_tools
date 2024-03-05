1bc2d50697319ca68bb84ba7bde31ebf00ba9aa7
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-03-05 14:35:07 +0100

    rm gradio

--- code_completion/main.py
+++ code_completion/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from deploy_service import gr, gr_app
+#from deploy_service import gr, gr_app
+from src.model_inference_cpp import run_code_completion, run_code_generation
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -8,4 +9,26 @@ app = FastAPI()
 def read_main():
     return {"message": "Welcome to REMIX-IDE AI services"}
 
-app = gr.mount_gradio_app(app, gr_app, path="/ai")
+@app.post("/ai/api/code_completion")
+def code_completion(data: dict):
+    context_code = data["data"][0]
+    comment = data["data"][1]
+    stream_result = data["data"][2]
+    max_new_tokens = data["data"][3]
+    temperature = data["data"][4]
+    top_p = data["data"][5]
+    top_k = data["data"][6]
+    return run_code_completion(context_code, comment, stream_result, max_new_tokens, temperature, top_p, top_k)
+
+@app.post("/ai/api/code_generation")
+def code_generation(data: dict):
+    gen_comment = data["data"][0]
+    stream_result = data["data"][1]
+    max_new_tokens = data["data"][2]
+    temperature = data["data"][3]
+    top_p = data["data"][4]
+    top_k = data["data"][5]
+    return run_code_generation(gen_comment, stream_result, max_new_tokens, temperature, top_p, top_k)
+
+
+#app = gr.mount_gradio_app(app, gr_app, path="/ai")
