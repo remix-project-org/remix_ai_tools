0b0bf8a2ba185fdf749f2a58f0f03a5e34a8603e
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-03-18 12:44:44 +0100

    fixed issue

--- services/src/llm_output_parser.py
+++ services/src/llm_output_parser.py
@@ -50,6 +50,6 @@ def get_string_between(source, start_str, end_str):
     end_index = source.find(end_str, start_index)
 
     if end_index == -1:
-        return source[start_index:-1]
+        return source[start_index:len(source)]
 
     return source[start_index:end_index]
