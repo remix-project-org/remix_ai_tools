5b82bba7c6a429a8cc240046f6b740780b4dc20b
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-04-02 12:13:57 +0200

    fixed llamacpp version to stabil

--- download_models.sh
+++ download_models.sh
@@ -1,3 +1,3 @@
-huggingface-cli download TheBloke/deepseek-coder-1.3b-instruct-GGUF deepseek-coder-1.3b-instruct.Q4_K_M.gguf --local-dir ~/ --local-dir-use-symlinks False
-huggingface-cli download TheBloke/deepseek-coder-6.7B-instruct-GGUF deepseek-coder-6.7b-instruct.Q4_K_M.gguf --local-dir ~/ --local-dir-use-symlinks False
-huggingface-cli download TheBloke/Mistral-7B-Instruct-v0.2-code-ft-GGUF mistral-7b-instruct-v0.2-code-ft.Q4_K_M.gguf --local-dir ~/ --local-dir-use-symlinks False
+huggingface-cli download TheBloke/deepseek-coder-1.3b-instruct-GGUF deepseek-coder-1.3b-instruct.Q4_K_M.gguf --local-dir ./.. --local-dir-use-symlinks False
+huggingface-cli download TheBloke/deepseek-coder-6.7B-instruct-GGUF deepseek-coder-6.7b-instruct.Q4_K_M.gguf --local-dir ./.. --local-dir-use-symlinks False
+huggingface-cli download TheBloke/Mistral-7B-Instruct-v0.2-code-ft-GGUF mistral-7b-instruct-v0.2-code-ft.Q4_K_M.gguf --local-dir ./.. --local-dir-use-symlinks False
