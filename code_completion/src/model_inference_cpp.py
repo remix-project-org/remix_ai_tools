0962d1eff05fc1fd70bc0037c528864d18b1ee45
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-04-10 15:38:27 +0200

    updated context

--- code_completion/src/model_inference_cpp.py
+++ code_completion/src/model_inference_cpp.py
@@ -10,7 +10,7 @@ completion_model = Llama(
   #model_path="../../deepseek-coder-6.7b-instruct.Q4_K_M.gguf" if use_deep_seek else "../../mistral-7b-instruct-v0.2-code-ft.Q4_K_M.gguf", 
   n_threads=16,           
   n_gpu_layers=-1,
-  n_ctx=2048,
+  n_ctx=4096,
   verbose=False
 )
 
