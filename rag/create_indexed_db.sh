bbd6273305b21bce8dc121ed1269885e30c1a289
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-04-23 15:25:59 +0200

    install script

--- rag/create_indexed_db.sh
+++ rag/create_indexed_db.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+apt-get update
 
-atlas deployments search indexes create --file indexDef-vector.json
-atlas deployments search indexes create --file indexDef-vector-cosine.json
+apt-get install gnupg curl
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+
+apt-get update
+sudo apt-get install -y mongodb-org
+
+wget -qO - https://pgp.mongodb.com/server-7.0.asc | sudo apt-key add -
+#atlas deployments search indexes create --file indexDef-vector.json
+#atlas deployments search indexes create --file indexDef-vector-cosine.json
