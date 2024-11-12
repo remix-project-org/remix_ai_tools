c86b7faed3dffc10034559b6dd3a4fcb40b7b346
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-11-12 10:48:52 +0000

    rm response time on stream

--- src/services/main.py
+++ src/services/main.py
@@ -1,12 +1,9 @@
-import os, sys, time, logging
+import os, sys
 sys.path.append('..')
 from src.entry import app
 from src.model_inference_cpp_flask import code_completion, code_explaining, code_insertion
 from src.model_inference_cpp_flask import error_explaining, solidity_answer, vulnerability_check
-from flask import request, Response, g
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 servertype = os.getenv("SERVERTYPE", 'fastapi')
 
 if servertype == 'flask':
@@ -21,21 +18,6 @@ if servertype == 'flask':
 def read_main():
     print("Welcome to REMIX-IDE AI services")
     return {"message": "Welcome to REMIX-IDE AI services"}
-
-
-@app.before_request
-def start_timer():
-    """Start the timer before processing a request."""
-    g.start_time = time.time()
-
-@app.after_request
-def log_response(response):
-    """Log the response time after processing a request."""
-    if hasattr(g, 'start_time'):
-        duration = time.time() - g.start_time
-        logger.info(f"Request: {request.method} {request.path} - Response time: {duration:.4f}s")
-        response.headers['X-Process-Time'] = str(duration)  # Add time to response headers
-    return response
 
 if __name__ == "__main__":
     app.run()
