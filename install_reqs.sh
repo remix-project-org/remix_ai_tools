e948a5c977ad7141d31f04c5fc6f6d403825aa61
STetsing <41009393+STetsing@users.noreply.github.com>
2025-01-28 14:08:43 +0100

    reduced reqs

--- install_reqs.sh
+++ install_reqs.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
-CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python==0.2.77
+CMAKE_ARGS="-DLLAMA_CUDA=on" FORCE_CMAKE=1 pip install llama-cpp-python==0.2.77
 export HF_HUB_ENABLE_HF_TRANSFER=1
