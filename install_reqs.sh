daefeb3da3e764b045808b3f97ea0c4ae5ca8e2c
STetsing <41009393+STetsing@users.noreply.github.com>
2025-01-28 14:07:59 +0100

    reduced reqs

--- install_reqs.sh
+++ install_reqs.sh
@@ -1,9 +1,3 @@
 pip install -r requirements.txt
 CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python==0.2.77
 export HF_HUB_ENABLE_HF_TRANSFER=1
-solc-select install all 
-
-apt install postgresql-client postgresql postgresql-contrib -y
-apt install -y postgresql-common
-/usr/share/postgresql-common/pgdg/apt.postgresql.org.sh 
-apt install postgresql-14-pgvector
