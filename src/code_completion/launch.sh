c0ba7e5f0b7e876d29f7cd522fe294361d98e3dc
STetsing <41009393+STetsing@users.noreply.github.com>
2025-02-17 17:55:57 +0100

    added launch multigpu

--- src/code_completion/launch.sh
+++ src/code_completion/launch.sh
@@ -4,9 +4,9 @@ total_workers=4
 app_module="main:app"
 
 for i in $(seq 1 $total_workers); do
+    echo "Starting worker $i"
     if (( i % 4 == 1 )); then
         CUDA_VISIBLE_DEVICES=1 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --timeout=30 --bind 0.0.0.0:7861 $app_module 
-
     elif (( i % 4 == 2 )); then
         CUDA_VISIBLE_DEVICES=2 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --timeout=30 --bind 0.0.0.0:7862 $app_module 
     elif (( i % 4 == 3 )); then
