61b5fe7102c210a39f7aac2a5c75b7373fae55a1
STetsing <41009393+STetsing@users.noreply.github.com>
2025-02-17 17:59:45 +0100

    added launch multigpu

--- src/code_completion/launch.sh
+++ src/code_completion/launch.sh
@@ -6,13 +6,13 @@ app_module="main:app"
 for i in $(seq 1 4); do
     echo "Starting worker $i"
     if (( i % 4 == 1 )); then
-        CUDA_VISIBLE_DEVICES=1 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --timeout=30 --bind 0.0.0.0:7861 $app_module 
+        CUDA_VISIBLE_DEVICES=1 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --timeout=30 --bind 0.0.0.0:7861 --timeout 600 $app_module 
     elif (( i % 4 == 2 )); then
-        CUDA_VISIBLE_DEVICES=2 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --timeout=30 --bind 0.0.0.0:7862 $app_module 
+        CUDA_VISIBLE_DEVICES=2 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --timeout=30 --bind 0.0.0.0:7862 --timeout 600 $app_module 
     elif (( i % 4 == 3 )); then
-        CUDA_VISIBLE_DEVICES=3 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --timeout=30 --bind 0.0.0.0:7863 $app_module 
+        CUDA_VISIBLE_DEVICES=3 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --timeout=30 --bind 0.0.0.0:7863 --timeout 600 $app_module 
     else
-        CUDA_VISIBLE_DEVICES=0 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --timeout=30 --bind 0.0.0.0:7864 $app_module  
+        CUDA_VISIBLE_DEVICES=0 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --timeout=30 --bind 0.0.0.0:7864 --timeout 600 $app_module  
     fi
     sleep 1
 done
