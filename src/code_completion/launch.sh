901942af8b90746224a44789bca2fded630b8300
STetsing <41009393+STetsing@users.noreply.github.com>
2025-02-17 18:02:35 +0100

    fix seq

--- src/code_completion/launch.sh
+++ src/code_completion/launch.sh
@@ -3,7 +3,7 @@
 total_workers=4
 app_module="main:app"
 
-for i in $(seq 1 4); do
+for ((i=1; i<=4; i++)); do
     echo "Starting worker $i"
     if (( i % 4 == 1 )); then
         CUDA_VISIBLE_DEVICES=1 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --timeout=30 --bind 0.0.0.0:7861 --timeout 600 $app_module 
