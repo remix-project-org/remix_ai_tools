653f97b7aefefadc71365134f62c6820fc685f1d
STetsing <41009393+STetsing@users.noreply.github.com>
2025-02-17 18:08:50 +0100

    minor

--- src/code_completion/launch.sh
+++ src/code_completion/launch.sh
@@ -3,5 +3,5 @@
 total_workers=4
 app_module="main:app"
 
-CUDA_VISIBLE_DEVICES=1 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --bind 0.0.0.0:7861 --timeout 600 $app_module && CUDA_VISIBLE_DEVICES=2 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --bind 0.0.0.0:7862 --timeout 600 $app_module && CUDA_VISIBLE_DEVICES=3 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --bind 0.0.0.0:7863 --timeout 600 $app_module && CUDA_VISIBLE_DEVICES=0 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --bind 0.0.0.0:7864 --timeout 600 $app_module &&
+CUDA_VISIBLE_DEVICES=1 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --bind 0.0.0.0:7861 --timeout 600 $app_module & CUDA_VISIBLE_DEVICES=2 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --bind 0.0.0.0:7862 --timeout 600 $app_module & CUDA_VISIBLE_DEVICES=3 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --bind 0.0.0.0:7863 --timeout 600 $app_module & CUDA_VISIBLE_DEVICES=0 SERVERTYPE=flask gunicorn --workers=7 --threads=20 --bind 0.0.0.0:7864 --timeout 600 $app_module &
 wait
